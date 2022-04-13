1551773a4d854fa07716a906297a88d96c95026d
Nick <nrlenz@gmail.com>
2022-04-13 09:36:39 -0500

    4/12/22 updates

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -8,8 +9,10 @@ namespace ContosoUniversity.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Display(Name = "Number")]
         public int CourseID { get; set; }
+
         [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
         [Range(0, 5)]
         public int Credits { get; set; }
 
